266ddfe7d236ab08401891009081529a1d129517
wair21 <wair21@gmail.com>
2019-10-08 23:17:46 +0300

    first step

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList =() => {
+    return (
+        <ul>
+            <li> Learn React</li>
+            <li> Build awesome app</li>
+        </ul>
+    );
+};
+
+const AppHeader= () => {
+    return (
+        <h1> My Todo List </h1>
+    );
+};
+
+const SearchPanel = () => {
+  return (
+      <input placeholder='search'/>
+  );
+};
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList/>
+        </div>
+    );
+};
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
